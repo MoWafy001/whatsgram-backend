7a2089e106383639c6d5e651e9ea48a0655aa69a
mowafy001 <wafy123445@gmail.com>
2022-02-12 22:53:58 +0200

    Get Messages, stickers, and images

--- whatsapp/socket.js
+++ whatsapp/socket.js
@@ -1,8 +1,9 @@
-const {handel_request_chats} = require('./socket_util')
+const su = require('./socket_util')
 
 const handel_whatsapp_socket = (client, socket) => {
 
-    socket.on('whatsapp-request-chats', handel_request_chats(client, socket))
+    socket.on('whatsapp-request-chats', su.handel_request_chats(client, socket))
+    socket.on('whatsapp-request-chat-messages', su.handel_request_chat_messages(client, socket))
 
 } 
 
--- whatsapp/socket_util.js
+++ whatsapp/socket_util.js
@@ -1,31 +1,42 @@
-
-
+// getting the list of chats
 const handel_request_chats = (client, socket) => {
     return async () => {
         let chats = await client.getChats();
-    
-    
         chats = await Promise.all(chats.map(async chat => {
             const chat_contact = await chat.getContact();
-            const profile_picture_url = await chat_contact.getProfilePicUrl().catch(_=>'');
+            const profile_picture_url = await chat_contact.getProfilePicUrl().catch(_ => '');
             const last_message = (await chat.fetchMessages({ limit: 1 }))[0];
-    
             const data = {
                 chat,
                 last_message,
                 profile_picture_url,
             }
-    
             return data
         }))
-    
         socket.emit('whatsapp-request-chats-done', chats)
     }
 }
 
 
+// getting the messages of a chat
+const handel_request_chat_messages = (client, socket) => {
+    return async chat_id => {
+        console.log(`${chat_id} requested`);
+        let chat = await client.getChatById(chat_id);
+        let messages = await chat.fetchMessages({ limit: 100 });
+        messages = await Promise.all(messages.map(async m => {
+            if(m.type === 'sticker' || m.type == 'image')
+                m.media = await m.downloadMedia()
+
+            return m
+        }))
+        socket.emit('whatsapp-request-chat-messages-done', messages)
+    }
+}
 
 
-
-
-module.exports={handel_request_chats}
+// exports
+module.exports = {
+    handel_request_chats,
+    handel_request_chat_messages,
+}
